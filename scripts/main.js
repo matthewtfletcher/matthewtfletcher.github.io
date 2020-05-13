464bc0f554184fa93d6733a724d7f183e6d78fbd
MFletcher <65247350+matthewtfletcher@users.noreply.github.com>
2020-05-13 15:20:09 -0400

    Updates

--- scripts/main.js
+++ scripts/main.js
@@ -23,7 +23,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  myHeading.textContent = 'Math is cool, ' + storedName;
 }
 myButton.onclick = function() {
   setUserName();
